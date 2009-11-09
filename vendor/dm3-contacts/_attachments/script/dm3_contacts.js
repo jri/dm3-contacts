815494eacbb373cfbb7b4483ff443fac6f7b273f
Jörg Richter <jri@deepamehta.de>
2009-11-09 17:04:27 +0100

    1) Adapted to current DeepaMehta 3 version. 2) An ant build file is included to support the plugin developer.

--- vendor/dm3-contacts/_attachments/script/dm3_contacts.js
+++ vendor/dm3-contacts/_attachments/script/dm3_contacts.js
@@ -1,81 +1,19 @@
 function dm3_contacts() {
-    this.name = "dm3-contacts"
-    // css_stylesheet("vendor/dm3-workspaces/style/dm3-workspaces.css")
-}
 
-dm3_contacts.prototype = {
-
-    init: function() {
+    this.init = function() {
         types["Contact"] = {
             fields: [
-                {
-                    id: "Name",
-                    model: {
-                        type: "text",
-                    },
-                    view: {
-                        editor: "single line"
-                    },
-                    content: ""
-                },
-                {
-                    id: "Notes",
-                    model: {
-                        type: "text",
-                    },
-                    view: {
-                        editor: "multi line",
-                        lines: 4
-                    },
-                    content: ""
-                },
-                {
-                    id: "Address",
-                    model: {
-                        type: "text",
-                    },
-                    view: {
-                        editor: "multi line",
-                        lines: 4
-                    },
-                    content: ""
-                },
-                {
-                    id: "Phone",
-                    model: {
-                        type: "text",
-                    },
-                    view: {
-                        editor: "multi line",
-                        lines: 2
-                    },
-                    content: ""
-                },
-                {
-                    id: "Email",
-                    model: {
-                        type: "text",
-                    },
-                    view: {
-                        editor: "multi line",
-                        lines: 2
-                    },
-                    content: ""
-                },
-                {
-                    id: "Website",
-                    model: {
-                        type: "text",
-                    },
-                    view: {
-                        editor: "multi line",
-                        lines: 2
-                    },
-                    content: ""
-                }
+                {id: "Name",    model: {type: "text"}, view: {editor: "single line"},          content: ""},
+                {id: "Notes",   model: {type: "text"}, view: {editor: "multi line", lines: 4}, content: ""},
+                {id: "Address", model: {type: "text"}, view: {editor: "multi line", lines: 4}, content: ""},
+                {id: "Phone",   model: {type: "text"}, view: {editor: "multi line", lines: 2}, content: ""},
+                {id: "Email",   model: {type: "text"}, view: {editor: "multi line", lines: 2}, content: ""},
+                {id: "Website", model: {type: "text"}, view: {editor: "multi line", lines: 2}, content: ""}
             ],
-            implementation: "PlainDocument",
-            icon_src: "vendor/dm3-contacts/images/bird.png"
+            view: {
+                icon_src: "vendor/dm3-contacts/images/bird.png"
+            },
+            implementation: "PlainDocument"
         }
     }
 }
