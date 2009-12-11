11299de4200e449a51a7f3aa45921351de7f0506
Jörg Richter <jri@deepamehta.de>
2009-12-11 04:08:07 +0100

    Person and institution "Notes" are rich text.

--- vendor/dm3-contacts/_attachments/script/dm3_contacts.js
+++ vendor/dm3-contacts/_attachments/script/dm3_contacts.js
@@ -7,7 +7,7 @@ function dm3_contacts() {
             {id: "Email",   model: {type: "text"}, view: {editor: "multi line", lines: 2}, content: ""},
             {id: "Website", model: {type: "text"}, view: {editor: "multi line", lines: 2}, content: ""},
             {id: "Address", model: {type: "text"}, view: {editor: "multi line", lines: 4}, content: ""},
-            {id: "Notes",   model: {type: "text"}, view: {editor: "multi line", lines: 4}, content: ""}
+            {id: "Notes",   model: {type: "html"}, view: {editor: "multi line", lines: 4}, content: ""}
         ],
         view: {
             icon_src: "vendor/dm3-contacts/images/bird.png"
@@ -21,7 +21,7 @@ function dm3_contacts() {
             {id: "Email",   model: {type: "text"}, view: {editor: "multi line", lines: 2}, content: ""},
             {id: "Website", model: {type: "text"}, view: {editor: "multi line", lines: 2}, content: ""},
             {id: "Address", model: {type: "text"}, view: {editor: "multi line", lines: 4}, content: ""},
-            {id: "Notes",   model: {type: "text"}, view: {editor: "multi line", lines: 4}, content: ""}
+            {id: "Notes",   model: {type: "html"}, view: {editor: "multi line", lines: 4}, content: ""}
         ],
         view: {
             icon_src: "vendor/dm3-contacts/images/birdhouse.png"
